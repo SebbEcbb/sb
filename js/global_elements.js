8e9509b508223611c1b17878d1163a502162f5e8
Sebb <ecbbadel@gmail.com>
2024-02-15 23:27:34 +0100

    dynamic-navbar

---
+++ js/global_elements.js
@@ -0,0 +1,4 @@
+function siteNavbar() {
+    document.getElementById("navbar").innerHTML = 
+    "<li class='menuitem'> <a href='#page-description' >Content</a> </li><li class='menuitem'> <a href='https://youtube.com/@sebastienbadelpiano' target='_blank'>YouTube</a> </li><li class='menuitem'> <a href='./contact.html' target='_blank'>Contact</a></li>";
+  }
