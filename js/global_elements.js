7d028811da2bb396c45c4c30b10b55a5aaefcb5e
Sebb <ecbbadel@gmail.com>
2024-02-16 10:07:37 +0100

    dynamic-js

--- js/global_elements.js
+++ js/global_elements.js
@@ -1,4 +1,14 @@
+function siteTitle() {
+  document.getElementById("site-title").innerHTML = 
+  "<a href='https://sebastienbadel.com'><img src='../Images/site-icon.JPG' height='30px' alt='Flower'>Sébastien Badel</a>"  
+}
+
 function siteNavbar() {
     document.getElementById("navbar").innerHTML = 
-    "<li class='menuitem'> <a href='#page-description' >Content</a> </li><li class='menuitem'> <a href='https://youtube.com/@sebastienbadelpiano' target='_blank'>YouTube</a> </li><li class='menuitem'> <a href='./contact.html' target='_blank'>Contact</a></li>";
+    "<li class='menuitem'><a href='#page-description' >Content</a><ul class='submenu'><li><a href='#top'>section 1</a></li><li><a href='#top'>section 1</a></li><li><a href='#top'>section 1</a></li></ul> </li><li class='menuitem'> <a href='https://youtube.com/@sebastienbadelpiano' target='_blank'>YouTube</a> </li><li class='menuitem'> <a href='./contact.html' target='_blank'>Contact</a></li>";
   }
+
+  function siteFooter() {
+    document.getElementById("site-title").innerHTML = 
+    "<a href='https://sebastienbadel.com'><img src='../Images/site-icon.JPG' height='30px' alt='Flower'>Sébastien Badel</a>"  
+  }  
