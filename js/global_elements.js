02219e9bc276759319708570833f2f6c1dc17549
Sebb <ecbbadel@gmail.com>
2024-02-22 18:22:13 +0100

    w

--- js/global_elements.js
+++ js/global_elements.js
@@ -17,3 +17,14 @@ function GlobalNavbar() {
     document.getElementById("donate").innerHTML = 
     "<form style='text-align: left; margin-top:5px' action='https://www.paypal.com/donate' method='post' target='_blank' > <input type='hidden' name='hosted_button_id' value='SDTTCGFJ7ZRMN' /> <input type='image' src='../Images/Heart.jpg' border='0' class='center' name='submit' title='PayPal - The safer, easier way to pay online!' alt='Bouton Faites un don avec PayPal' /></form>"
   }  
+
+  
+  function HomeDonate() {
+    document.getElementById("donate").innerHTML = 
+    "<form style='text-align: left; margin-top:5px' action='https://www.paypal.com/donate' method='post' target='_blank' > <input type='hidden' name='hosted_button_id' value='SDTTCGFJ7ZRMN' /> <input type='image' src='./Images/Heart.jpg' border='0' class='center' name='submit' title='PayPal - The safer, easier way to pay online!' alt='Bouton Faites un don avec PayPal' /></form>"
+  }  
+
+  function HomeNavbar() {
+    document.getElementById("navbar").innerHTML = 
+    "<li class='menuitem'> <a href='./contact.html' target='_blank'>Contact</a></li><li class='menuitem'> <a href='https://youtube.com/@sebastienbadelpiano' target='_blank'>YouTube</a> </li><li class='menuitem'><a href='#page-description' >Shop</a></li><li class='menuitem'><a href='#page-description' >Blog</a></li><li class='menuitem'><a href='#page-description' >Contenu</a></li><li class='menuitem'><a href='#page-description' >Home</a></li>";
+  }
