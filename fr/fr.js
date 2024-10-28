9cb415dd64895008db11d0f02b7c1387597071b0
Sebb <ecbbadel@gmail.com>
2024-10-28 16:51:23 +0100

    w

--- fr/fr.js
+++ fr/fr.js
@@ -1,4 +1,5 @@
 function loadJs_fr() {
+  /*alert("ok")*/
     siteTitle();
     navigation_fr();
     siteFooter_fr();
@@ -6,25 +7,26 @@ function loadJs_fr() {
 }
 
 function siteFooter_fr() {
-    document.getElementById("footer").innerHTML = 
-      "<div class='s100'></div>"+        
-      "<a href='../contact.html' class='links' target='_blank'><img style='vertical-align:middle;margin-right: 5px;' src='../Images/message_icon.png'' height='24px' alt='message-icon'><strong>Contact</strong></a>&nbsp &nbsp;"+
-      "<a href='https://www.linkedin.com/in/sebastienbadelpiano/' class='links' target='_blank'> <img style='vertical-align:middle;margin-right: 5px;' src='../Images/li_icon.png'' height='24px' alt='message-icon'><strong>LinkedIn </strong> </a>&nbsp &nbsp;"+
-      "<a href='https://youtube.com/@SebastienBadelPiano'class='links' target='_blank'>  <img style='vertical-align:middle;margin-right: 5px;' src='../Images/yt_icon.png'' height='24px' alt='message-icon'> <strong>Youtube </strong></a>&nbsp &nbsp; "+                
-      "<div class='s50'></div><p  style='text-align: center;'>Site dévéloppé ©2020-2024 par Sébastien Badel</p>"
-  } 
+  document.getElementById("footer").innerHTML = 
+    "<div class='s100'></div>"+        
+    "<a href='./contact.html' class='links' target='_blank'><img style='vertical-align:middle;margin-right: 5px;' src='../Images/message_icon.png'' height='24px' alt='message-icon'><strong>Contact</strong></a>&nbsp &nbsp;"+
+    "<a href='https://www.linkedin.com/in/sebastienbadelpiano/' class='links' target='_blank'> <img style='vertical-align:middle;margin-right: 5px;' src='../Images/li_icon.png'' height='24px' alt='message-icon'><strong>LinkedIn </strong> </a>&nbsp &nbsp;"+
+    "<a href='https://youtube.com/@SebastienBadelPiano'class='links' target='_blank'>  <img style='vertical-align:middle;margin-right: 5px;' src='../Images/yt_icon.png'' height='24px' alt='message-icon'> <strong>Youtube </strong></a>&nbsp &nbsp; "+                
+    "<div class='s50'></div><p  style='text-align: center;'>Site developpé ©2020-2024 par Sébastien Badel</p>"
+} 
+
 function navigation_fr() {
     document.getElementById("navigation").innerHTML = 
     "<li class='menuitem'><a href='./contact.html' target='_blank'>Me contacter</a></li>"+
     "<li class='menuitem'><a href='https://youtube.com/@sebastienbadelpiano' target='_blank'>YouTube</a> </li>"+
     "<li class='menuitem'><a href='./boutique' target='_blank' >Trucs à vendre</a></li>"+
     "<li class='menuitem'><a href='./blog.html' target='_blank'>Articles</a></li>"+
-    "<li class='menuitem'><a href='#page-description' >Contenu de page</a></li>"+  
+    "<li class='menuitem'><a href='#page-description' >Contenu de page</a></li>"
   }
 
   function donation_fr() {
     document.getElementById("donate").innerHTML = 
-    " <p><strong>Voulez-vous m'aider ?</strong></p><br><form style='text-align: left; margin-top:5px' action='https://www.paypal.com/donate' method='post' target='_blank' > <input type='hidden' name='hosted_button_id' value='SDTTCGFJ7ZRMN' /> <input type='image' src='../Images/Heart.jpg' border='0' width='30%' name='submit' title='PayPal - The safer, easier way to pay online!' alt='Faire un don avec PayPal' /></form>"
+    " <p><strong>Voulez-vous m'aider ?</strong></p><br><form style='text-align: left; margin-top:5px' action='https://www.paypal.com/donate' method='post' target='_blank' > <input type='hidden' name='hosted_button_id' value='SDTTCGFJ7ZRMN' /> <input type='image' src='../Images/Heart.jpg' border='0' width='30%' name='submit' title='PayPal - The safer, easier way to pay online!' alt='Button make a donation with PayPal' /></form>"
   } 
 
   function StickyJs() {
